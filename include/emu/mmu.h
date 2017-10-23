d8692be2065de13eab2d669fba71c690d945669c
Gary Guo <gary@garyguo.net>
2017-10-23 22:48:52 +0100

    Implement MMU for the interpreter.

---
+++ include/emu/mmu.h
@@ -0,0 +1,174 @@
+#ifndef EMU_MMU_H
+#define EMU_MMU_H
+
+#include <cstddef>
+#include <stdexcept>
+#include <tuple>
+#include <unordered_map>
+
+#include "config.h"
+#include "emu/typedef.h"
+#include "util/assert.h"
+#include "util/memory.h"
+
+namespace emu {
+
+static constexpr reg_t page_size = 0x1000;
+static constexpr reg_t page_mask = page_size - 1;
+static constexpr reg_t log_page_size = 12;
+
+class Mmu {
+public:
+    virtual ~Mmu() {}
+
+private:
+    // Used internally to implement copy_*_host and zero_memory.
+    template<typename T>
+    void blockify(reg_t address, size_t size, T&& lambda);
+
+public:
+    virtual std::byte* translate_page(reg_t address) = 0;
+    
+    // Get the size of continuous block starting from the given address.
+    virtual size_t get_block_size(reg_t address) = 0;
+
+    virtual void allocate_page(reg_t address, reg_t size) = 0;
+
+    template<typename T>
+    T load_memory_aligned(reg_t address) {
+        reg_t page_offset = address & page_mask;
+        reg_t page_base = address &~ page_mask;
+        std::byte *translated_page = translate_page(page_base);
+        return util::interpret_as<T>(translated_page + page_offset);
+    }
+
+    template<typename T>
+    void store_memory_aligned(reg_t address, T value) {
+        reg_t page_offset = address & page_mask;
+        reg_t page_base = address &~ page_mask;
+        std::byte *translated_page = translate_page(page_base);
+        util::interpret_as<T>(translated_page + page_offset) = value;
+    }
+
+    // To make the interface obvious and tidy, these functions are defined as templates. However the function lies on a
+    // very cold path, and is relatively complex. Putting it inside header file might cause compiler to inline the file
+    // and/or make the compilation time longer.
+    template<typename T>
+    T load_memory_misaligned(reg_t address);
+
+    template<typename T>
+    void store_memory_misaligned(reg_t address, T value);
+
+    template<typename T>
+    T load_memory(reg_t address) {
+        constexpr reg_t addr_mask = sizeof(T) - 1;
+        if (LIKELY((address & addr_mask) == 0)) {
+            return load_memory_aligned<T>(address);
+        } else {
+            return load_memory_misaligned<T>(address);
+        }
+    }
+
+    template<typename T>
+    void store_memory(reg_t address, T value) {
+        constexpr reg_t addr_mask = sizeof(T) - 1;
+        if (LIKELY((address & addr_mask) == 0)) {
+            return store_memory_aligned<T>(address, value);
+        } else {
+            return store_memory_misaligned<T>(address, value);
+        }
+    }
+
+    void copy_from_host(reg_t address, void* source, size_t size);
+    void copy_to_host(reg_t address, void* target, size_t size);
+    void zero_memory(reg_t address, size_t size);
+};
+
+class Paging_mmu final: public Mmu {
+public:
+
+    // These fields are exposed as public intentionally for now, since they are needed by the code generator.
+    mutable reg_t cached_query[32];
+    mutable std::byte* cached_result[32];
+    std::unordered_map<reg_t, std::byte*> map;
+
+    Paging_mmu() {
+
+        // All valid entries stored in TLB will be page-aligned.
+        // Since the bit pattern of -1 is page-aligned, the TLB is effectively marked as invalid.
+        for (auto& query: cached_query) query = -1;
+    }
+
+    // Free all the mappings. Placed in mmu.cc since it contains munmap.
+    ~Paging_mmu();
+
+private:
+    // Fetch the mapping of address and place it into TLB.
+    void tlb_fetch(reg_t address);
+
+public:
+    // Translating address of a page. The address must be page-aligned.
+    virtual std::byte* translate_page(reg_t address) override {
+
+        // The input must be page-aligned.
+        PRECONDITION((address & page_mask) == 0);
+
+        const ptrdiff_t tag = (address >> log_page_size) & 31;
+
+        if (UNLIKELY(cached_query[tag] != address)) tlb_fetch(address);
+
+        return cached_result[tag];
+    }
+
+    // Page is the basic unit in Paging_mmu.
+    virtual size_t get_block_size(reg_t address) override {
+        return page_size - (address & page_mask);
+    }
+
+    // Allocate memory at given virtual address and estabilish mapping.
+    // The address and size must be page-aligned.
+    virtual void allocate_page(reg_t address, reg_t size) override;
+};
+
+class Flat_mmu final: public Mmu {
+public:
+
+    // These fields are exposed as public intentionally for now, since they are needed by the code generator.
+    std::byte *memory_;
+    size_t size_;
+
+    // Similarly, placed in mmu.cc since it contains mmap/munmap.
+    Flat_mmu(size_t size);
+    ~Flat_mmu();
+
+public:
+    // Translating address of a page. The address must be page-aligned.
+    virtual std::byte* translate_page(reg_t address) override {
+
+        // The input must be page-aligned.
+        PRECONDITION((address & page_mask) == 0);
+
+        if (UNLIKELY(address >= size_)) {
+            throw std::runtime_error {"Page fault"};
+        }
+
+        return memory_ + address;
+    }
+
+    // In Flat_mmu's case we divide the memory space into two regions: valid and invalid.
+    virtual size_t get_block_size(reg_t address) override {
+        if (address < size_) {
+            return size_ - address;
+        } else {
+            return page_size - (address &~ page_mask);
+        }
+    }
+
+    // Allocate memory at given virtual address and estabilish mapping.
+    // The address and size must be page-aligned.
+    virtual void allocate_page(reg_t address, reg_t size) override;
+};
+
+}
+
+#endif
---
+++ src/emu/mmu.cc
@@ -0,0 +1,182 @@
+#include <cstring>
+#include <sys/mman.h>
+
+#include "emu/mmu.h"
+
+namespace emu {
+
+template<typename T>
+T Mmu::load_memory_misaligned(reg_t address) {
+    reg_t page_offset = address & page_mask;
+    reg_t page_base = address &~ page_mask;
+    std::byte *translated_page = translate_page(page_base);
+
+    if (UNLIKELY(page_offset > page_size - sizeof(T))) {
+
+        // Unaligned, cross-page access. Slow path
+        std::byte word[sizeof(T)];
+
+        std::byte *next_translated_page = translate_page(page_base + page_size);
+        int size_in_page = page_size - page_offset;
+        memcpy(word, translated_page + page_offset, size_in_page);
+        memcpy(word + size_in_page, next_translated_page, sizeof(T) - size_in_page);
+
+        return util::interpret_as<T>(word);
+    }
+
+    return util::interpret_as<T>(translated_page + page_offset);
+}
+
+template<typename T>
+void Mmu::store_memory_misaligned(reg_t address, T value) {
+    reg_t page_offset = address & page_mask;
+    reg_t page_base = address &~ page_mask;
+    std::byte *translated_page = translate_page(page_base);
+
+    if (UNLIKELY(page_offset > page_size - sizeof(T))) {
+
+        // Unaligned, cross-page access. Slow path
+        std::byte word[sizeof(T)];
+        util::interpret_as<T>(word) = value;
+
+        std::byte *next_translated_page = translate_page(page_base + page_size);
+        int size_in_page = page_size - page_offset;
+        memcpy(translated_page + page_offset, word, size_in_page);
+        memcpy(next_translated_page, word + size_in_page, sizeof(T) - size_in_page);
+        
+        return;
+    }
+
+    util::interpret_as<T>(translated_page + page_offset) = value;
+}
+
+template<typename T>
+void Mmu::blockify(reg_t address, size_t size, T&& lambda) {
+    while (size != 0) {
+        size_t block_size = get_block_size(address);
+        if (block_size >= size) {
+            lambda(address, size);
+            return;
+        }
+        lambda(address, block_size);
+        address += block_size;
+        size -= block_size;
+    }
+}
+
+void Mmu::copy_from_host(reg_t address, void* target, size_t size) {
+    std::byte *target_bytes = reinterpret_cast<std::byte*>(target);
+    blockify(address, size, [=](reg_t start, size_t limit) {
+        std::byte *buffer = translate_page(start &~ page_mask) + (start & page_mask);
+        memcpy(buffer, target_bytes + (start - address), limit);
+    });
+}
+
+void Mmu::copy_to_host(reg_t address, void* target, size_t size) {
+    std::byte *target_bytes = reinterpret_cast<std::byte*>(target);
+    blockify(address, size, [=](reg_t start, size_t limit) {
+        std::byte *buffer = translate_page(start &~ page_mask) + (start & page_mask);
+        memcpy(target_bytes + (start - address), buffer, limit);
+    });
+}
+
+void Mmu::zero_memory(reg_t address, size_t size) {
+    blockify(address, size, [=](reg_t start, size_t limit) {
+        std::byte *buffer = translate_page(start &~ page_mask) + (start & page_mask);
+        memset(buffer, 0, limit);
+    });
+}
+
+Paging_mmu::~Paging_mmu() {
+    
+    // Free any mapped pages.
+    for (const auto& mapping: map) {
+        munmap(mapping.second, page_size);
+    }
+}
+
+void Paging_mmu::tlb_fetch(reg_t address) {
+
+    // The input must be page-aligned.
+    PRECONDITION((address & page_mask) == 0);
+
+    const ptrdiff_t tag = (address >> log_page_size) & 31;
+
+    // Unlikely event of cache miss. Get the entry in the map.
+    const auto& mapping = map.find(address);
+
+    // If the mapping does not exist, it will be a page fault. However since currently we does not handle heap and
+    // stack correctly yet, we will map the page in RW
+    if (UNLIKELY(mapping == map.end())) {
+        // throw std::runtime_error {"Page fault"};
+        allocate_page(address, page_size);
+        cached_query[tag] = address;
+        cached_result[tag] = map.at(address);
+        return;
+    }
+
+    cached_query[tag] = address;
+    cached_result[tag] = mapping->second;
+}
+
+void Paging_mmu::allocate_page(reg_t address, reg_t size) {
+
+    // The input must be page-aligned.
+    PRECONDITION((address & page_mask) == 0 && (size & page_mask) == 0);
+
+    std::byte *result = reinterpret_cast<std::byte*>(
+        mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)
+    );
+
+    if (result == nullptr) {
+        throw std::bad_alloc {};
+    }
+
+    for (reg_t offset = 0; offset < size; offset += page_size) {
+        map[address + offset] = result + offset;
+    }
+}
+
+Flat_mmu::Flat_mmu(size_t size): size_(size) {
+
+    // Ideally we want the page to be all PROT_NONE, then allocate when it is needed. Since we cannot handle heap and
+    // stack correctly yet, we make them all RW.
+    memory_ = reinterpret_cast<std::byte*>(
+        mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)
+    );
+
+    if (memory_ == nullptr) {
+        throw std::bad_alloc {};
+    }
+}
+
+Flat_mmu::~Flat_mmu() {
+    munmap(memory_, size_);
+}
+
+// Allocate memory at given virtual address and estabilish mapping.
+// The address and size must be page-aligned.
+void Flat_mmu::allocate_page(reg_t address, reg_t size) {
+
+    // The input must be page-aligned.
+    PRECONDITION((address & page_mask) == 0 && (size & page_mask) == 0);
+
+    if (address >= size_) {
+        throw std::bad_alloc {};
+    }
+
+    if (mprotect(memory_ + address, size, PROT_READ | PROT_WRITE) == -1) {
+        throw std::bad_alloc {};
+    }
+}
+
+// As mentioned in mmu.h, we keep template interface for simplicity and tidyness, but the definition is in this file,
+// so to make these function accessible we have to explicit instantiate them.
+template uint16_t Mmu::load_memory_misaligned<uint16_t>(reg_t);
+template uint32_t Mmu::load_memory_misaligned<uint32_t>(reg_t);
+template uint64_t Mmu::load_memory_misaligned<uint64_t>(reg_t);
+template void Mmu::store_memory_misaligned<uint16_t>(reg_t, uint16_t);
+template void Mmu::store_memory_misaligned<uint32_t>(reg_t, uint32_t);
+template void Mmu::store_memory_misaligned<uint64_t>(reg_t, uint64_t);
+
+}
