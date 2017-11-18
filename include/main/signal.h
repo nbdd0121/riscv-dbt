8e227343cab6b079b8cf1411a9eb629d4cb0494c
Gary Guo <gary@garyguo.net>
2017-11-18 23:07:47 +0000

    Make Segv_exception derive from std::exception.

--- include/main/signal.h
+++ include/main/signal.h
@@ -1,8 +1,17 @@
 #ifndef MAIN_SIGNAL_H
 #define MAIN_SIGNAL_H
 
-class Segv_exception {};
-class Fpe_exception {};
+#include <stdexcept>
+
+class Segv_exception: public std::runtime_error {
+public:
+    Segv_exception(): std::runtime_error {"segmentation fault"} {}
+};
+
+class Fpe_exception: public std::runtime_error {
+public:
+    Fpe_exception(): std::runtime_error {"floating point exception"} {}
+};
 
 void setup_fault_handler();
 
