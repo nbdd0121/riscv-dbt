ee7b7da5d9e611b3d247d53ba788e6222e7f56c1
Gary Guo <gary@garyguo.net>
2017-12-07 23:11:38 +0800

    Refactor builder

--- include/ir/builder.h
+++ include/ir/builder.h
@@ -3,76 +3,71 @@
 
 #include "ir/instruction.h"
 
-namespace ir::builder {
+namespace ir {
 
-[[maybe_unused]]
-static Instruction* constant(Type type, uint64_t value) {
-    auto inst = new Instruction(type, Opcode::constant, {});
-    inst->attribute(value);
-    return inst;
-}
+class Builder {
+private:
+    Graph& _graph;
+public:
+    Builder(Graph& graph): _graph{graph} {}
 
-[[maybe_unused]]
-static Instruction* cast(Type type, bool sext, Instruction* operand) {
-    auto inst = new Instruction(type, Opcode::cast, {operand});
-    inst->attribute(sext);
-    return inst;
-}
+    Instruction* constant(Type type, uint64_t value) {
+        auto inst = new Instruction(type, Opcode::constant, {});
+        inst->attribute(value);
+        return _graph.manage(inst);
+    }
 
-[[maybe_unused]]
-static Instruction* i_return(Instruction* dep) {
-    auto inst = new Instruction(Type::none, Opcode::i_return, {dep});
-    return inst;
-}
+    Instruction* cast(Type type, bool sext, Instruction* operand) {
+        auto inst = new Instruction(type, Opcode::cast, {operand});
+        inst->attribute(sext);
+        return _graph.manage(inst);
+    }
 
-[[maybe_unused]]
-static Instruction* load_register(Instruction* dep, int regnum) {
-    auto inst = new Instruction(Type::i64, Opcode::load_register, {dep});
-    inst->attribute(regnum);
-    return inst;
-}
+    Instruction* i_return(Instruction* dep) {
+        return _graph.manage(new Instruction(Type::none, Opcode::i_return, {dep}));
+    }
 
-[[maybe_unused]]
-static Instruction* store_register(Instruction* dep, int regnum, Instruction* operand) {
-    auto inst = new Instruction(Type::i64, Opcode::store_register, {dep, operand});
-    inst->attribute(regnum);
-    return inst;
-}
+    Instruction* load_register(Instruction* dep, int regnum) {
+        auto inst = new Instruction(Type::i64, Opcode::load_register, {dep});
+        inst->attribute(regnum);
+        return _graph.manage(inst);
+    }
 
-[[maybe_unused]]
-static Instruction* load_memory(Instruction* dep, Type type, Instruction* address) {
-    return new Instruction(type, Opcode::load_memory, {dep, address});
-}
+    Instruction* store_register(Instruction* dep, int regnum, Instruction* operand) {
+        auto inst = new Instruction(Type::i64, Opcode::store_register, {dep, operand});
+        inst->attribute(regnum);
+        return _graph.manage(inst);
+    }
 
-[[maybe_unused]]
-static Instruction* store_memory(Instruction* dep, Instruction* address, Instruction* value) {
-    return new Instruction(Type::none, Opcode::store_memory, {dep, address, value});
-}
+    Instruction* load_memory(Instruction* dep, Type type, Instruction* address) {
+        return _graph.manage(new Instruction(type, Opcode::load_memory, {dep, address}));
+    }
 
-[[maybe_unused]]
-static Instruction* emulate(Instruction* dep, void* ptr) {
-    auto inst = new Instruction(Type::none, Opcode::emulate, {dep});
-    inst->attribute_pointer(ptr);
-    return inst;
-}
+    Instruction* store_memory(Instruction* dep, Instruction* address, Instruction* value) {
+        return _graph.manage(new Instruction(Type::none, Opcode::store_memory, {dep, address, value}));
+    }
 
-[[maybe_unused]]
-static Instruction* arithmetic(Opcode opcode, Instruction* left, Instruction* right) {
-    ASSERT(left->type() == right->type());
-    return new Instruction(left->type(), opcode, {left, right});
-}
+    Instruction* emulate(Instruction* dep, void* ptr) {
+        auto inst = new Instruction(Type::none, Opcode::emulate, {dep});
+        inst->attribute_pointer(ptr);
+        return _graph.manage(inst);
+    }
 
-[[maybe_unused]]
-static Instruction* shift(Opcode opcode, Instruction* left, Instruction* right) {
-    ASSERT(right->type() == Type::i8);
-    return new Instruction(left->type(), opcode, {left, right});
-}
+    Instruction* arithmetic(Opcode opcode, Instruction* left, Instruction* right) {
+        ASSERT(left->type() == right->type());
+        return _graph.manage(new Instruction(left->type(), opcode, {left, right}));
+    }
 
-[[maybe_unused]]
-static Instruction* compare(Opcode opcode, Instruction* left, Instruction* right) {
-    ASSERT(left->type() == right->type());
-    return new Instruction(Type::i1, opcode, {left, right});
-}
+    Instruction* shift(Opcode opcode, Instruction* left, Instruction* right) {
+        ASSERT(right->type() == Type::i8);
+        return _graph.manage(new Instruction(left->type(), opcode, {left, right}));
+    }
+
+    Instruction* compare(Opcode opcode, Instruction* left, Instruction* right) {
+        ASSERT(left->type() == right->type());
+        return _graph.manage(new Instruction(Type::i1, opcode, {left, right}));
+    }
+};
 
 }
 
