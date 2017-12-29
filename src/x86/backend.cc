nbdd0121/riscv-dbt

commit b881a45401b3fab12269104c48fb03751273602c

Extract is_int* and is_uint* to a separate header file.

src/x86/backend.cc
@@ -2,6 +2,7 @@
 #include "emu/state.h"
 #include "util/format.h"
 #include "util/functional.h"
+#include "util/int_size.h"
 #include "util/memory.h"
 #include "x86/backend.h"
 #include "x86/builder.h"
@@ -13,10 +14,6 @@ using namespace x86::builder;
 static constexpr int volatile_register[] = {0, 1, 2, 6, 7, 8, 9, 10, 11};
 
 static uint64_t spill_area[128];
-
-static bool is_int32(uint64_t value) {
-    return static_cast<uint64_t>(static_cast<int32_t>(value)) == value;
-}
 
 static int register_id(x86::Register reg) {
     ASSERT(reg != x86::Register::none);
@@ -246,7 +243,7 @@ void Backend::decrease_reference(ir::Value value) {
 
 Operand Backend::get_location(ir::Value value) {
     if (value.is_const()) {
-        ASSERT(is_int32(value.const_value()));
+        ASSERT(util::is_int32(value.const_value()));
         return value.const_value();
     }
     auto ptr = location.find(value);
@@ -256,7 +253,7 @@ Operand Backend::get_location(ir::Value value) {
 
 Operand Backend::get_location_ex(ir::Value value, bool allow_mem, bool allow_imm) {
     if (value.is_const()) {
-        if (allow_imm && is_int32(value.const_value())) {
+        if (allow_imm && util::is_int32(value.const_value())) {
             return value.const_value();
         }
         Register reg = alloc_register(value.type());
@@ -565,7 +562,8 @@ void Backend::after(ir::Node* node) {
                 bind_register(output, reg);
 
                 if (output.type() != ir::Type::i8) {
-                    emit(mov(modify_size(ir::Type::i32, reg), 0));
+                    Operand reg32 = modify_size(ir::Type::i32, reg);
+                    emit(i_xor(reg32, reg32));
                 }
                 emit(setcc(cc, modify_size(ir::Type::i8, reg)));
                 break;

include/ir/node.h
@@ -25,8 +25,7 @@ enum class Type: uint8_t {
     control = 0xFF,
 };
 
-[[maybe_unused]]
-static size_t get_type_size(Type type) {
+static inline size_t get_type_size(Type type) {
     return static_cast<uint8_t>(type);
 }
 
@@ -120,19 +119,16 @@ enum class Opcode: uint8_t {
     mux,
 };
 
-[[maybe_unused]]
-static bool is_pure_opcode(Opcode opcode) {
+static inline bool is_pure_opcode(Opcode opcode) {
     return static_cast<uint8_t>(opcode) >= static_cast<uint8_t>(Opcode::constant);
 }
 
-[[maybe_unused]]
-static bool is_binary_opcode(Opcode opcode) {
+static inline bool is_binary_opcode(Opcode opcode) {
     uint8_t value = static_cast<uint8_t>(opcode);
     return value >= static_cast<uint8_t>(Opcode::add) && value <= static_cast<uint8_t>(Opcode::geu);
 }
 
-[[maybe_unused]]
-static bool is_commutative_opcode(Opcode opcode) {
+static inline bool is_commutative_opcode(Opcode opcode) {
     switch(opcode) {
         case Opcode::add:
         case Opcode::i_xor:
@@ -173,13 +169,11 @@ public:
     inline uint64_t const_value() const;
 };
 
-[[maybe_unused]]
-static bool operator ==(Value a, Value b) {
+static inline bool operator ==(Value a, Value b) {
     return a.node() == b.node() && a.index() == b.index();
 }
 
-[[maybe_unused]]
-static bool operator !=(Value a, Value b) { return !(a == b); }
+static inline bool operator !=(Value a, Value b) { return !(a == b); }
 
 class Node {
 private:

include/util/int_size.h
@@ -1,0 +1,38 @@
+#ifndef UTIL_INT_SIZE_H
+#define UTIL_INT_SIZE_H
+
+#include <cstdint>
+
+namespace util {
+
+static inline bool is_int8(uint32_t imm) {
+    return static_cast<uint32_t>(static_cast<int32_t>(static_cast<int8_t>(imm))) == imm;
+}
+
+static inline bool is_int8(uint64_t imm) {
+    return static_cast<uint64_t>(static_cast<int64_t>(static_cast<int8_t>(imm))) == imm;
+}
+
+static inline bool is_int16(uint64_t imm) {
+    return static_cast<uint64_t>(static_cast<int64_t>(static_cast<int16_t>(imm))) == imm;
+}
+
+static inline bool is_int32(uint64_t imm) {
+    return static_cast<uint64_t>(static_cast<int64_t>(static_cast<int32_t>(imm))) == imm;
+}
+
+static inline bool is_uint8(uint64_t imm) {
+    return (imm & 0xFF) == imm;
+}
+
+static inline bool is_uint16(uint64_t imm) {
+    return (imm & 0xFFFF) == imm;
+}
+
+static inline bool is_uint32(uint64_t imm) {
+    return (imm & 0xFFFFFFFF) == imm;
+}
+
+}
+
+#endif

src/x86/encoder.cc
@@ -1,4 +1,5 @@
 #include "util/assert.h"
+#include "util/int_size.h"
 #include "util/memory.h"
 #include "x86/encoder.h"
 #include "x86/opcode.h"
@@ -6,35 +7,6 @@
 using namespace x86;
 
 namespace {
-
-// Need a separate copy for memory displacements.
-bool is_int8(uint32_t imm) {
-    return static_cast<uint32_t>(static_cast<int32_t>(static_cast<int8_t>(imm))) == imm;
-}
-
-bool is_int8(uint64_t imm) {
-    return static_cast<uint64_t>(static_cast<int64_t>(static_cast<int8_t>(imm))) == imm;
-}
-
-bool is_int16(uint64_t imm) {
-    return static_cast<uint64_t>(static_cast<int64_t>(static_cast<int16_t>(imm))) == imm;
-}
-
-bool is_int32(uint64_t imm) {
-    return static_cast<uint64_t>(static_cast<int64_t>(static_cast<int32_t>(imm))) == imm;
-}
-
-bool is_uint8(uint64_t imm) {
-    return (imm & 0xFF) == imm;
-}
-
-bool is_uint16(uint64_t imm) {
-    return (imm & 0xFFFF) == imm;
-}
-
-bool is_uint32(uint64_t imm) {
-    return (imm & 0xFFFFFFFF) == imm;
-}
 
 int get_size(const Operand& operand) {
     if (operand.is_register()) {
@@ -57,13 +29,13 @@ int get_size(const Operand& operand) {
 // If operation size is 8, 16 or 32-bit, then imm must be int8/int16/int32 or uint8/uint16/uint32.
 void check_immediate_size(int size, uint64_t imm) {
     if (size == 1) {
-        ASSERT(is_int8(imm) || is_uint8(imm));
+        ASSERT(util::is_int8(imm) || util::is_uint8(imm));
     } else if (size == 2) {
-        ASSERT(is_int16(imm) || is_uint16(imm));
+        ASSERT(util::is_int16(imm) || util::is_uint16(imm));
     } else if (size == 4) {
-        ASSERT(is_int32(imm) || is_uint32(imm));
+        ASSERT(util::is_int32(imm) || util::is_uint32(imm));
     } else if (size == 8) {
-        ASSERT(is_int32(imm));
+        ASSERT(util::is_int32(imm));
     } else {
         ASSERT(0);
     }
@@ -237,7 +209,7 @@ void Encoder::emit_modrm(const Operand& operand, Register reg) {
                 }
 
                 // [RSP/R12 + disp8]
-                if (is_int8(it.displacement)) {
+                if (util::is_int8(it.displacement)) {
                     emit_byte(0x40 | (reg_num << 3) | 0b100);
                     emit_byte(0x24);
                     emit_byte(static_cast<uint8_t>(it.displacement));
@@ -258,7 +230,7 @@ void Encoder::emit_modrm(const Operand& operand, Register reg) {
             }
 
             // [base + disp8]
-            if (is_int8(it.displacement)) {
+            if (util::is_int8(it.displacement)) {
                 emit_byte(0x40 | (reg_num << 3) | base_reg);
                 emit_byte(static_cast<uint8_t>(it.displacement));
                 return;
@@ -278,7 +250,7 @@ void Encoder::emit_modrm(const Operand& operand, Register reg) {
         }
 
         // [base + index * scale + disp8]
-        if (is_int8(it.displacement)) {
+        if (util::is_int8(it.displacement)) {
             emit_byte(0x40 | (reg_num << 3) | 0b100);
             emit_byte((shift << 6) | (index_reg << 3) | base_reg);
             emit_byte(static_cast<uint8_t>(it.displacement));
@@ -382,7 +354,7 @@ void Encoder::emit_alu(const Instruction& inst, int id) {
         check_immediate_size(op_size, imm);
 
         // Short encoding available for 8-bit immediate.
-        if (op_size != 1 && is_int8(imm)) {
+        if (op_size != 1 && util::is_int8(imm)) {
             emit_r_rm(op_size, dst, static_cast<Register>(id), 0x83);
             emit_byte(imm);
             return;
@@ -439,7 +411,7 @@ void Encoder::emit_shift(const Instruction& inst, int id) {
     // Otherwise src must be uint8.
     ASSERT(src.is_immediate());
     uint64_t imm = src.as_immediate();
-    ASSERT(is_uint8(imm));
+    ASSERT(util::is_uint8(imm));
 
     if (imm == 1) {
         emit_r_rm(op_size, dst, static_cast<Register>(id), op_size == 1 ? 0xD0 : 0xD1);
@@ -464,7 +436,7 @@ void Encoder::emit_call(const Instruction& inst) {
     }
 
     uint64_t imm = dst.as_immediate();
-    ASSERT(is_int32(imm));
+    ASSERT(util::is_int32(imm));
     emit_byte(0xE8);
     emit_dword(imm);
 }
@@ -491,13 +463,13 @@ void Encoder::emit_jcc(const Instruction& inst) {
     ASSERT(inst.operands[1].is_empty() && dst.is_immediate());
 
     uint64_t imm = dst.as_immediate();
-    if (is_int8(imm)) {
+    if (util::is_int8(imm)) {
         emit_byte(0x70 + static_cast<uint8_t>(inst.cond));
         emit_byte(imm);
         return;
     }
 
-    ASSERT(is_int32(imm));
+    ASSERT(util::is_int32(imm));
     emit_byte(0x0F);
     emit_byte(0x80 + static_cast<uint8_t>(inst.cond));
     emit_dword(imm);
@@ -518,13 +490,13 @@ void Encoder::emit_jmp(const Instruction& inst) {
     }
 
     uint64_t imm = dst.as_immediate();
-    if (is_int8(imm)) {
+    if (util::is_int8(imm)) {
         emit_byte(0xEB);
         emit_byte(imm);
         return;
     }
 
-    ASSERT(is_int32(imm));
+    ASSERT(util::is_int32(imm));
     emit_byte(0xE9);
     emit_dword(imm);
 }
@@ -552,11 +524,11 @@ void Encoder::emit_mov(const Instruction& inst) {
         if (dst.is_register()) {
 
             // Special optimization for mov: mov rax, uint32 can be optimized to mov eax, uint32.
-            if (op_size == 8 && is_uint32(imm)) op_size = 4;
+            if (op_size == 8 && util::is_uint32(imm)) op_size = 4;
 
             // If the above optimization is not possible, but imm is int32, then it is shorter to encode it using
             // mod r/m.
-            if (op_size != 8 || !is_int32(imm)) {
+            if (op_size != 8 || !util::is_int32(imm)) {
                 emit_plusr(op_size, dst.as_register(), op_size == 1 ? 0xB0 : 0xB8);
                 emit_immediate(op_size, imm);
                 return;
@@ -669,11 +641,11 @@ void Encoder::emit_push(const Instruction& inst) {
     // push imm. Note that we does not support push word xxx.
     if (inst.operands[0].is_immediate()) {
         uint64_t imm = inst.operands[0].as_immediate();
-        if (is_int8(imm)) {
+        if (util::is_int8(imm)) {
             emit_byte(0x6A);
             emit_byte(imm);
         } else {
-            ASSERT(is_int32(imm));
+            ASSERT(util::is_int32(imm));
             emit_byte(0x68);
             emit_dword(imm);
         }
@@ -704,7 +676,7 @@ void Encoder::emit_ret(const Instruction& inst) {
     if (!inst.operands[0].is_empty()) {
         ASSERT(inst.operands[0].is_immediate() && inst.operands[1].is_empty());
         imm = inst.operands[1].as_immediate();
-        ASSERT(is_uint16(imm));
+        ASSERT(util::is_uint16(imm));
     }
 
     if (imm == 0) {

commit 2e98e7feec2e4a87bb37b29da5af211ee20b12c2

Fix a bug mistakenly introduced by b881a45.

src/x86/backend.cc
@@ -555,7 +555,6 @@ void Backend::after(ir::Node* node) {
 
             // Special handling for i1 upcast
             if (op.type() == ir::Type::i1) {
-                Condition_code cc = emit_compare(op);
 
                 // Allocate and bind register.
                 Register reg = alloc_register(output.type());
@@ -565,6 +564,12 @@ void Backend::after(ir::Node* node) {
                     Operand reg32 = modify_size(ir::Type::i32, reg);
                     emit(i_xor(reg32, reg32));
                 }
+
+                // Compare instruction must be generated after xor, as xor will modify flags.
+                pin_register(reg);
+                Condition_code cc = emit_compare(op);
+                unpin_register(reg);
+
                 emit(setcc(cc, modify_size(ir::Type::i8, reg)));
                 break;
             }